c4d29bd1dec93220c778170a4c85f70b7767f427
Victor M. Rivas <vrivas@ujaen.es>
2023-04-04 18:41:37 +0200

    TDD para aniadirClase

--- front-end/static-files/js/front-end-spec.js
+++ front-end/static-files/js/front-end-spec.js
@@ -63,7 +63,42 @@ describe("Frontend.Article.actualizar: ", function () {
 })
 
 
-// Víctor: 4-abr-2023. TDD Para quitarClase y aniadirClase
+// Víctor: 4-abr-2023. TDD Para aniadirClase y quitarClase
+describe("Frontend.aniadirClase: ", function () {
+    it("no da error con un elemento que NO tiene ninguna clase",
+        function () {
+            const elemento = document.getElementById("seccion-principal")
+            const nombreClase = "clase-patata"
+            expect(Frontend.aniadirClase(elemento, nombreClase)).toBe(Frontend)
+            expect(elemento.getAttribute("class")).toBe(nombreClase)
+
+            // Devolvemos el elemento a su estado original:
+            elemento.setAttribute("class", "")
+        })
+
+    it("no da error con un elemento que tiene la misma clase que queremos añadir",
+        function () {
+            const elemento = document.getElementsByTagName("a")[0] // primer enlace de NAV
+            const nombreClase = "opcion-principal"
+            const claseAnterior = elemento.getAttribute("class")
+            expect(Frontend.aniadirClase(elemento, nombreClase)).toBe(Frontend)
+            expect(elemento.getAttribute("class")).toBe(claseAnterior)
+        })
+
+    it("no da error con un elemento que NO tiene la clase que queremos añadir",
+        function () {
+            const elemento = document.getElementsByTagName("a")[0] // primer enlace de NAV
+            const nombreClase = "clase-patata"
+            const claseAnterior = elemento.getAttribute("class")
+            expect(Frontend.aniadirClase(elemento, nombreClase)).toBe(Frontend)
+            expect(elemento.getAttribute("class").includes( claseAnterior)).toBe(true)
+            expect(elemento.getAttribute("class").includes( nombreClase)).toBe(true)
+
+            // Devolvemos el elemento a su estado original:
+            elemento.setAttribute("class", claseAnterior )
+        })
+})
+
 describe("Frontend.quitarClase: ", function () {
     it("no da error con un elemento que NO tiene ninguna clase",
         function () {
@@ -74,7 +109,7 @@ describe("Frontend.quitarClase: ", function () {
         })
     it("no da error con un elemento que tiene alguna clase, pero no la que queremos quitar",
         function () {
-            const elemento = document.getElementsByTagName( "a" )[0] // primer enlace de NAV
+            const elemento = document.getElementsByTagName("a")[0] // primer enlace de NAV
             const nombreClase = "clase-patata"
             const claseAnterior = elemento.getAttribute("class")
             expect(Frontend.quitarClase(elemento, nombreClase)).toBe(Frontend)
@@ -82,14 +117,15 @@ describe("Frontend.quitarClase: ", function () {
         })
     it("no da error con un elemento que tiene la clase que queremos quitar",
         function () {
-            const elemento = document.getElementsByTagName( "a" )[0] // primer enlace de NAV
+            const elemento = document.getElementsByTagName("a")[0] // primer enlace de NAV
             const nombreClase = "opcion-principal"
             const claseAnterior = elemento.getAttribute("class")
             expect(Frontend.quitarClase(elemento, nombreClase)).toBe(Frontend)
             expect(elemento.getAttribute("class")).not.toBe(claseAnterior)
 
             // Devolvemos el elemento a su estado original:
-            Frontend.aniadirClase( elemento, nombreClase)
+            Frontend.aniadirClase(elemento, nombreClase)
         })
 
 })
+
